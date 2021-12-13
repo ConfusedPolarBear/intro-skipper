2445823c5af39bffb1667c873c1a96b1289189bc
Cody Robibero <cody@robibe.ro>
2021-12-13 16:58:05 -0700

    update editorconfig, add analyzers

--- Jellyfin.Plugin.Template/Configuration/PluginConfiguration.cs
+++ Jellyfin.Plugin.Template/Configuration/PluginConfiguration.cs
@@ -1,28 +1,57 @@
 using MediaBrowser.Model.Plugins;
 
-namespace Jellyfin.Plugin.Template.Configuration
+namespace Jellyfin.Plugin.Template.Configuration;
+
+/// <summary>
+/// The configuration options.
+/// </summary>
+public enum SomeOptions
 {
-    public enum SomeOptions
+    /// <summary>
+    /// Option one.
+    /// </summary>
+    OneOption,
+
+    /// <summary>
+    /// Second option.
+    /// </summary>
+    AnotherOption
+}
+
+/// <summary>
+/// Plugin configuration.
+/// </summary>
+public class PluginConfiguration : BasePluginConfiguration
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PluginConfiguration"/> class.
+    /// </summary>
+    public PluginConfiguration()
     {
-        OneOption,
-        AnotherOption
+        // set default options here
+        Options = SomeOptions.AnotherOption;
+        TrueFalseSetting = true;
+        AnInteger = 2;
+        AString = "string";
     }
 
-    public class PluginConfiguration : BasePluginConfiguration
-    {
-        // store configurable settings your plugin might need
-        public bool TrueFalseSetting { get; set; }
-        public int AnInteger { get; set; }
-        public string AString { get; set; }
-        public SomeOptions Options { get; set; }
+    /// <summary>
+    /// Gets or sets a value indicating whether some true or false setting is enabled..
+    /// </summary>
+    public bool TrueFalseSetting { get; set; }
 
-        public PluginConfiguration()
-        {
-            // set default options here
-            Options = SomeOptions.AnotherOption;
-            TrueFalseSetting = true;
-            AnInteger = 2;
-            AString = "string";
-        }
-    }
+    /// <summary>
+    /// Gets or sets an integer setting.
+    /// </summary>
+    public int AnInteger { get; set; }
+
+    /// <summary>
+    /// Gets or sets a string setting.
+    /// </summary>
+    public string AString { get; set; }
+
+    /// <summary>
+    /// Gets or sets an enum option.
+    /// </summary>
+    public SomeOptions Options { get; set; }
 }
--- Jellyfin.Plugin.Template/Plugin.cs
+++ Jellyfin.Plugin.Template/Plugin.cs
@@ -1,36 +1,51 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using Jellyfin.Plugin.Template.Configuration;
 using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
 
-namespace Jellyfin.Plugin.Template
+namespace Jellyfin.Plugin.Template;
+
+/// <summary>
+/// The main plugin.
+/// </summary>
+public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
 {
-    public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Plugin"/> class.
+    /// </summary>
+    /// <param name="applicationPaths">Instance of the <see cref="IApplicationPaths"/> interface.</param>
+    /// <param name="xmlSerializer">Instance of the <see cref="IXmlSerializer"/> interface.</param>
+    public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
+        : base(applicationPaths, xmlSerializer)
     {
-        public override string Name => "Template";
+        Instance = this;
+    }
 
-        public override Guid Id => Guid.Parse("eb5d7894-8eef-4b36-aa6f-5d124e828ce1");
+    /// <inheritdoc />
+    public override string Name => "Template";
 
-        public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer) : base(applicationPaths, xmlSerializer)
+    /// <inheritdoc />
+    public override Guid Id => Guid.Parse("eb5d7894-8eef-4b36-aa6f-5d124e828ce1");
+
+    /// <summary>
+    /// Gets the current plugin instance.
+    /// </summary>
+    public static Plugin? Instance { get; private set; }
+
+    /// <inheritdoc />
+    public IEnumerable<PluginPageInfo> GetPages()
+    {
+        return new[]
         {
-            Instance = this;
-        }
-
-        public static Plugin Instance { get; private set; }
-
-        public IEnumerable<PluginPageInfo> GetPages()
-        {
-            return new[]
+            new PluginPageInfo
             {
-                new PluginPageInfo
-                {
-                    Name = this.Name,
-                    EmbeddedResourcePath = string.Format("{0}.Configuration.configPage.html", GetType().Namespace)
-                }
-            };
-        }
+                Name = this.Name,
+                EmbeddedResourcePath = string.Format(CultureInfo.InvariantCulture, "{0}.Configuration.configPage.html", GetType().Namespace)
+            }
+        };
     }
 }
