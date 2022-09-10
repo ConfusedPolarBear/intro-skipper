d8bea7b54ef43eafc8cef7c48329d36e506a0248
ConfusedPolarBear <33811686+ConfusedPolarBear@users.noreply.github.com>
2022-09-10 02:24:44 -0500

    Ignore virtual items and log more info

--- ConfusedPolarBear.Plugin.IntroSkipper/QueueManager.cs
+++ ConfusedPolarBear.Plugin.IntroSkipper/QueueManager.cs
@@ -136,6 +136,7 @@ public class QueueManager
             },
             IncludeItemTypes = new BaseItemKind[] { BaseItemKind.Episode },
             Recursive = true,
+            IsVirtualItem = false
         };
 
         _logger.LogDebug("Getting items");
@@ -174,7 +175,11 @@ public class QueueManager
 
         if (string.IsNullOrEmpty(episode.Path))
         {
-            _logger.LogWarning("Not queuing episode {Id} as no path was provided by Jellyfin", episode.Id);
+            _logger.LogWarning(
+                "Not queuing episode \"{Name}\" from series \"{Series}\" ({Id}) as no path was provided by Jellyfin",
+                episode.Name,
+                episode.SeriesName,
+                episode.Id);
             return;
         }
 
